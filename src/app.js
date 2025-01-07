e933572639ef04429497e6471ab9cb294027b008
Eshwar <eshwar25in@gmail.com>
2025-01-07 21:31:58 +0530

    dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -3,16 +3,28 @@ const connectDB = require("./config/database")
 const app = express()
 const User = require("./models/user")
 
+//this app.use handles all routes get post put delete 
+//express.json() middleware converts the json data recieved to javascript object 
+app.use(express.json())
+
+//inserting a dummy data into our database 
+//app.post is used to add or insert the data into our databse
 app.post("/signup",async(req,res)=>{
-    const user = new User({
-        firstName : "Jamboju",
-        lastname: "Eshwar",
-        emailId:"eshwar25in@gmail.com",
-        password:"es21012003"
-    })
+
+    //creating user model dynamically
+
+    //thats it we can insert user data dynamically
+    const user = new User(req.body);
+    console.log(req.body);
+
+    // const user = new User({
+    //     firstName : "Jamboju",
+    //     lastname: "Eshwar",
+    //     emailId:"eshwar25in@gmail.com",
+    //     password:"es21012003"
+    // })
     
-    try{
-        
+    try{    
        await user.save();
       res.send("User Added Successfully")
     }catch(err){
@@ -22,6 +34,7 @@ app.post("/signup",async(req,res)=>{
 })
 
 connectDB().then(()=>{
+// always first connect to the database and then start listening to requests
     console.log("Database connection established...")
     app.listen(7777,()=>{
         console.log("Server is successfully listening on port 7777")
@@ -31,5 +44,7 @@ connectDB().then(()=>{
 .catch((err)=>{
     console.log("Database cannot be connected!!!")
 })
+
+
 
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose')
 
-
+//this is the correct way to handle the connection using async function
 const  connectDB = async ()=>{
     
-    await mongoose.connect("mongodb+srv://eshwar:VU5CGD7q3AGMBnfH@namastenode.cgsl7.mongodb.net/")
+    await mongoose.connect("mongodb+srv://eshwar:VU5CGD7q3AGMBnfH@namastenode.cgsl7.mongodb.net/devTinder")
 
 }
 
 module.exports = connectDB;
+
+
+ //this mongoose.connect returns a promise and also tells us that connection is established successfully or not
+ //this is not correct way to handle 
+// mongoose.connect(
+//     "mongodb+srv://eshwar:VU5CGD7q3AGMBnfH@namastenode.cgsl7.mongodb.net/devTinder"
+// )
+
